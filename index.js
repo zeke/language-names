1f150ad7489865cc2e2bde3429544edc7dc164bb
Zeke Sikelianos <zeke@sikelianos.com>
2021-02-10 15:57:29 -0800

    add html output

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const ISO6391 = require('iso-639-1')
-
+const hubdown = require('hubdown')
 const locales = ISO6391.getAllCodes()
+let output = ''
 
-for (const locale of locales) {
-  console.log(`\n\n## ${locale}\n\n`)
-
-  for (const sublocale of locales) {
-    const getEnglishName = new Intl.DisplayNames('en', { type: 'language' })
-    const getName = new Intl.DisplayNames(sublocale, { type: 'language' })
-    console.log('-', getName.of(locale), `(${getEnglishName.of(sublocale)})`)
-  }
-  console.log('\n---\n')
+function log(line) {
+  // console.log(line)
+  output = output + line + '\n'
 }
+
+async function main () {
+  
+  for (const locale of locales) {
+    const getEnglishName = new Intl.DisplayNames('en', { type: 'language' })
+    log(`\n\n## ${locale}\n\n`)
+  
+    for (const sublocale of locales) {
+      const getName = new Intl.DisplayNames(sublocale, { type: 'language' })
+      log(`- ${getName.of(locale)} (${getEnglishName.of(sublocale)})`)
+    }
+    log('\n---\n')
+  }
+
+  // console.log(output)
+  const { content } = await hubdown(output)
+  console.log(content)
+}
+
+
+main()
